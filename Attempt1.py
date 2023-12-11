57efa16f767e75fe19fbced6400290e14bff151d
vjones715 <153471965+vjones715@users.noreply.github.com>
2023-12-10 21:02:40 -0500

    Add files via upload

---
+++ Attempt1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: ValerieMJones
+"""
+
+get ipython().system('pip install camelot-py')
+import camelot
+tables = camelot.read_pdf('rpam.pdf')
+tables
+<TableList n=1>
+tables.export('foo.csv', f='csv', compress=True) # json, excel, html
+tables[0]
+<Table shape=(7, 7)>
+tables[0].parsing_report
+{
+    'accuracy': 99.02,
+    'whitespace': 12.24,
+    'order': 1,
+    'page': 1
+}
+tables[0].to_csv('foo.csv') # to_json, to_excel, to_html
+tables[0].df # get a pandas DataFrame!
